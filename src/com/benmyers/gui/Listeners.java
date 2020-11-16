ceaa54cd87bd152a41add668b6ae58342614756e
benben28405678 <benben28405678@gmail.com>
2020-11-16 14:47:12 -0800

    Replaced returned ActionListeners in Listeners.java with lambdas

--- src/com/benmyers/gui/Listeners.java
+++ src/com/benmyers/gui/Listeners.java
@@ -7,28 +7,17 @@ import javax.swing.*;
 public class Listeners {
 
     public static ActionListener translateButtonListener(JTextArea inputTextArea, JTextArea outputTextArea, JTextArea logTextArea, JCheckBox enableLoggingCheckBox) {
-        return new ActionListener() {
+        return e -> {
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                if(enableLoggingCheckBox.isSelected()) {
-                    Events.translate(inputTextArea, outputTextArea, logTextArea);
-                } else {
-                    Events.translate(inputTextArea, outputTextArea);
-                }
+            if(enableLoggingCheckBox.isSelected()) {
+                Events.translate(inputTextArea, outputTextArea, logTextArea);
+            } else {
+                Events.translate(inputTextArea, outputTextArea);
             }
         };
     }
 
     public static ActionListener translateAndLogButtonListener(JTextArea inputTextArea, JTextArea outputTextArea, JTextArea logTextArea) {
-        return new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                Events.translateAndLog(inputTextArea, outputTextArea, logTextArea);
-            }
-        };
+        return e -> Events.translateAndLog(inputTextArea, outputTextArea, logTextArea);
     }
 }
